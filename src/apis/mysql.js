a80ccfd72c8d3a5907f5a8d049d3d8733b8cb191
avansover <amir@vansover.com>
2023-07-09 12:13:42 +0300

    added sql connection

--- src/apis/mysql.js
+++ src/apis/mysql.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,21 +20,40 @@ const sqlConnection = mysql_1.default.createConnection({
     password: "eV4IUrhQZH",
     database: "sql7630477"
 });
-const sqlQueryMaker = () => {
-    sqlConnection.connect(function (err) {
-        if (err)
-            throw err;
-        console.log("Connected!");
-        sqlConnection.query("SELECT * FROM Characters", function (err, result) {
-            if (err)
-                throw err;
-            console.log("Result: " + result);
-            sqlConnection.end(function (err) {
-                if (err)
-                    throw err;
-                console.log("Connection closed.");
+const sqlQueryMaker = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield new Promise((resolve, reject) => {
+            sqlConnection.connect(err => {
+                if (err) {
+                    console.error('Connection error:', err);
+                    reject(err);
+                    return;
+                }
+                console.log("Connected!");
+                const query = "SELECT * FROM Characters";
+                sqlConnection.query(query, (err, result) => {
+                    if (err) {
+                        console.error('Query error:', err);
+                        reject(err);
+                        return;
+                    }
+                    console.log("Result:", result);
+                    resolve(result);
+                });
+                sqlConnection.end(err => {
+                    if (err) {
+                        console.error('Connection closure error:', err);
+                    }
+                    else {
+                        console.log("Connection closed.");
+                    }
+                });
             });
         });
-    });
-};
+    }
+    catch (error) {
+        console.log("sqlQueryMaker error");
+        console.log(error);
+    }
+});
 exports.sqlQueryMaker = sqlQueryMaker;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -14,12 +23,18 @@ app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded());
 app.use('/api/user', userRouter_1.default);
-// var con = mysql.createConnection({
-//     host: "sql7.freesqldatabase.com",
-//     user: "sql7630477",
-//     password: "eV4IUrhQZH"
-// });
-(0, mysql_1.sqlQueryMaker)();
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield (0, mysql_1.sqlQueryMaker)();
+            // Perform actions after the query has completed successfully
+        }
+        catch (error) {
+            // Handle the error
+        }
+    });
+}
+fetchData();
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 });
